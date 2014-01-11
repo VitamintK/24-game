00249268820d2a081abfb2a7277ed944aa5bc8bb
KEvin Wang <kvn.a.wng@gmail.com>
2014-01-10 16:55:44 -0800

    Initial commit
    Initial commit

---
+++ twentyfour.py
@@ -0,0 +1,21 @@
+import itertools
+def tryall (a, current):
+    #print (a,current)
+    return [(current+a,"+"),(current-a,"-"),(current*a,"*"),(current/a,"/")]
+def tf (thelist, current, curpath=""):
+    if (len(thelist)>0):
+        a = thelist[0]
+        rest = thelist[1:]
+        for i in tryall(a,current):
+            tf (rest,i[0],curpath+i[1]+str(a))
+        #tf([thelist[1]]+thelist[2:]+[thelist[0]],current)
+    else:
+        if current == 24:
+            print curpath
+            True
+        else:
+        	False
+def aux(a,b):
+    tf(a[1:],a[0],str(a[0]))
+for perm in itertools.permutations([5.0,7.0,6.0,3.0]):
+    aux(perm,0)
